df54a43cdd2468e284b1162605b10615bec328ee
Evgeny Cherepukhin <gruop10@yandex.ru>
2022-02-19 12:52:41 +0300

    4 sprint Final project

---
+++ search-server/string_processing.cpp
@@ -0,0 +1,26 @@
+#include<algorithm>
+
+#include"string_processing.h"
+
+using namespace std;
+
+VectorString SplitIntoWords(const string& text) {
+    vector<string> words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        }
+        else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+
+    return words;
+}
---
+++ search-server/string_processing.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+
+using VectorString = std::vector<std::string>;
+
+ VectorString SplitIntoWords(const std::string& text);
